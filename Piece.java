9bdfec7ebf135885b215cd5702dfa2185e7ca0c4
Mwahahahamud <110490132+Mwahahahamud@users.noreply.github.com>
2022-11-29 07:48:40 +0000

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -39,8 +39,6 @@ computeKnightNextPositions(): Put into Knight class
 
 public abstract class Piece {
 
-
-
 //  public static int KING_VALUE = 10000;
 
   protected Position position;
@@ -66,167 +64,12 @@ public abstract class Piece {
      Q -> queen
   */
 
-
-  /*
-  public List<Position> validNextPositions() {
-    List<Position> nextPositions = new ArrayList<>();
-
-//    position.getAllMoves();
-
-    switch (name) {
-      case 'K':
-        {
-          position.getAllDiagonalMoves(1, board(), nextPositions);
-          position.getAllStraightMoves(1, board(), nextPositions);
-          break;
-        }
-      case 'P':
-        computePawnNextPositions(nextPositions);
-        break;
-      case 'B':
-        position.getAllDiagonalMoves(8, board(), nextPositions);
-        break;
-      case 'N':
-        computeKnightNextPositions(nextPositions);
-        break;
-      case 'R':
-        position.getAllStraightMoves(8, board(), nextPositions);
-        break;
-      case 'Q':
-        {
-          position.getAllDiagonalMoves(8, board(), nextPositions);
-          position.getAllStraightMoves(8, board(), nextPositions);
-          break;
-        }
-    }
-
-    return nextPositions;
-  }
-
-   */
-
-
   public abstract char icon();
 
-  /*
-  public char icon() {
-
-    boolean colourSwitch = pieceColor == PieceColor.BLACK;
-
-    switch (name) {
-      case 'K':
-        return colourSwitch ? '♚' : '♔';
-      case 'P':
-        return colourSwitch ? '♟' : '♙';
-      case 'B':
-        return colourSwitch ? '♝' : '♗';
-      case 'N':
-        return colourSwitch ? '♞' : '♘';
-      case 'R':
-        return colourSwitch ? '♜' : '♖';
-      case 'Q':
-        return colourSwitch ? '♛' : '♕';
-      default:
-        return 0;
-    }
-  }
-
-   */
-
+  
   public abstract int value();
-
-  /*
-  public int value() {
-    switch (name) {
-      case 'K':
-        return KING_VALUE;
-      case 'P':
-        return 1;
-      case 'B':
-        return 3;
-      case 'N':
-        return 3;
-      case 'R':
-        return 5;
-      case 'Q':
-        return 10;
-      default:
-        return 0;
-    }
-    // the king's value must be some high value, but it must be less than
-    //  integer max value - queen value (such that we can compute the effectiveness of a move
-    // that takes a queen and also puts the king in check at the same time).
-  }
-
-   */
-
-  /*
-
-  private void computeKnightNextPositions(List<Position> nextPositions) {
-    // directions a knight can travel in.
-    final int[][] nextPosDeltas =
-        new int[][] {
-          {1, 2}, {1, -2}, {-1, 2}, {-1, -2},
-          {2, 1}, {-2, 1}, {2, -1}, {-2, -1}
-        };
-
-    // iterate through all possible positions, getting any valid next positions
-    for (int[] posDeltaPair : nextPosDeltas) {
-      position.addPosAtDelta(posDeltaPair[0], posDeltaPair[1], board(), nextPositions);
-    }
-  }
-
-  private void computePawnNextPositions(List<Position> nextPositions) {
-    // The En passant move is not included.
-    // The Promotion is not included.
-
-
-//    pawns can move up 1 if it is a non occupied square.
-//    pawns can move (and take) up 1 and left or right 1 if the square is occupied by an opponent
-//    pawns can move up 2 if they are currently on their home row (pawns cannot go backwards)
-
-
-
-    int upDir = (pieceColor == WHITE ? 1 : -1);
-
-    // move up by one
-    addPawnPositionIfValid(upDir, 0, false, nextPositions);
-
-    // move up left and right
-    addPawnPositionIfValid(upDir, -1, true, nextPositions);
-    addPawnPositionIfValid(upDir, 1, true, nextPositions);
-
-    // move up two if on their home row
-    if (position().getRank() == (colour() == BLACK ? SEVEN : TWO)) {
-      addPawnPositionIfValid(upDir + upDir, 0, false, nextPositions);
-    }
-  }
-
-  private void addPawnPositionIfValid(
-      int deltaRank,
-      int deltaFile,
-      boolean allowIfOccupiedByOpponent,
-      List<Position> nextPositions) {
-
-    Position movePosition = position().getPosAtDelta(deltaRank, deltaFile);
-
-    if (movePosition != null) {
-      boolean movePosOccupied = board().positionOccupied(movePosition);
-      // the up left and right cases
-      if (allowIfOccupiedByOpponent
-          && movePosOccupied
-          && board().atPosition(movePosition).colour() != colour()) {
-        nextPositions.add(movePosition);
-      }
-      // the "up straight ahead" and "up straight ahead two" moves.
-      else if (!allowIfOccupiedByOpponent && !movePosOccupied) {
-        nextPositions.add(movePosition);
-      }
-    }
-  }
-
-
-  */
+  
+ 
   public Position position() {
     return position;
   }
